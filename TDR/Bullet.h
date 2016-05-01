c48c9d3c359b8acad0192e4bac99851dede84f21
bluebear94 <tkook11@gmail.com>
2016-05-01 12:36:39 -0400

    Start work on bullets

---
+++ TDR/Bullet.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <stdint.h>
+
+namespace tdr {
+	typedef struct { int32_t u; } fix1616;
+	fix1616 operator*(fix1616 a, fix1616 b) {
+		int64_t prod = (((int64_t) a.u) * b.u) >> 16;
+		if (prod >= 0x100000000) prod = 0xFFFFFFFF;
+		if (prod < -((int64_t) 0x100000000)) prod = -((int64_t) 0xFFFFFFFF);
+		return { (int32_t) prod };
+	}
+	fix1616 operator/(fix1616 a, fix1616 b) {
+		int64_t aex = (((int64_t) a.u) << 16);
+		return { (int32_t) (aex / b.u) };
+	}
+	struct Bullet {
+		fix1616 x, y;
+		fix1616 xs, ys;
+		fix1616 xa, ya;
+		fix1616 speed, angle, angularVelocity;
+		// Width and length would be the same for ordinary bullets,
+		// but different for lasers.
+		fix1616 visualWidth, collisionWidth;
+		fix1616 visualLength, collisionLength;
+		uint16_t left, top, right, bottom; // texcoords
+		// Hopefully no one wants to graze anything less often than
+		// 128 frames.
+		//
+		// n > 0 -> every n frames
+		// 0 -> only once
+		// -1 -> never
+		int8_t grazeFrequency;
+		// n > 0 -> in n frames
+		// 0 -> ready
+		// -1 -> can't graze anymore
+		int8_t timeToNextGraze;
+		int isLaser : 1;
+		int markedForDeletion : 1;
+		int radialAndRectangularDetached : 1;
+	};
+}
