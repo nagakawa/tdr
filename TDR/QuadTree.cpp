112ae791e7687fd1353a0350efb61f50e53535fc
bluebear94 <tkook11@gmail.com>
2016-06-25 01:29:52 -0400

    Actually make cmake compile QuadTree.cpp

--- TDR/QuadTree.cpp
+++ TDR/QuadTree.cpp
@@ -24,42 +24,71 @@ tdr::QuadTreeNode::~QuadTreeNode() {
 void tdr::QuadTreeNode::subdivide() {
   assert(nw == nullptr);
   float q = halfDim / 2;
-  nw = new QuadTree(this, x - q, y - q, q);
-  ne = new QuadTree(this, x + q, y - q, q);
-  sw = new QuadTree(this, x - q, y + q, q);
-  se = new QuadTree(this, x + q, y + q, q);
+  nw = new QuadTreeNode(this, x - q, y - q, q);
+  ne = new QuadTreeNode(this, x + q, y - q, q);
+  sw = new QuadTreeNode(this, x - q, y + q, q);
+  se = new QuadTreeNode(this, x + q, y + q, q);
   for (int i = 0; i < numPoints; ++i)
     insertInChild(points[i]);
 }
 
-bool tdr::QuadTree::contains(float x, float y) {
+bool tdr::QuadTreeNode::contains(float x, float y) {
   float nx = x - this->x;
   float ny = y - this->y;
-  if (nx < -halfDim || nx > halfDim || ny < -halfDim || ny > halfDim)
+  if (nx >= -halfDim || nx < halfDim || ny >= -halfDim || ny < halfDim)
     return false;
   return true;
 }
 
-void tdr::QuadTree::percolateRadiusIncrease(float r) {
+void tdr::QuadTreeNode::percolateRadiusIncrease(float r) {
   if (r > maxRadius) maxRadius = r;
   else return;
   if (parent != nullptr) parent->percolateRadiusIncrease(r);
 }
 
-bool tdr::QuadTree::insert(Circle& c) {
-  if (!contains(c.x, c.y)) return false;
+bool tdr::QuadTreeNode::insert(QuadTreeLeaf& h) {
+  if (!contains(h.c.x, h.c.y)) return false;
   if (numPoints < MAX_QT_NODES) {
-    points[numPoints++] = c;
-    percolateRadiusIncrease(c.radius);
+    points[numPoints++] = h;
+    percolateRadiusIncrease(h.c.radius);
     return true;
   }
   if (nw == nullptr) subdivide();
-  insertInChild(c);
+  insertInChild(h);
   return true;
 }
 
-void tdr::QuadTree::insertInChild(Circle& c) {
-  if (!(nw->insert(c) || ne->insert(c) || sw->insert(c) || se.insert(c))) {
+// TODO compaction
+bool tdr::QuadTreeNode::remove(float x, float y, QuadTreeLeaf* st) {
+  if (!contains(x, y)) return false;
+  if (nw == nullptr) { // No children
+    for (int i = 0; i < numPoints; ++i) {
+      // Remove the point, and stash it if necessary.
+      if (points[i].c.x == x && points[i].c.y == y) {
+        --numPoints;
+        if (st != nullptr) *st = points[i];
+        points[i] = points[numPoints];
+        return true;
+      }
+    }
+    // No appropriate point was found.
+    return false;
+  } else { // there are children
+    if (x < this->x) {
+      if (y < this->y) return nw->remove(x, y, st);
+      else return sw->remove(x, y, st);
+    } else {
+      if (y < this->y) return ne->remove(x, y, st);
+      else return se->remove(x, y, st);
+    }
+  }
+  fprintf(stderr, "Not placed in a child?\n");
+  abort();
+  return false;
+}
+
+void tdr::QuadTreeNode::insertInChild(QuadTreeLeaf& h) {
+  if (!(nw->insert(h) || ne->insert(h) || sw->insert(h) || se->insert(h))) {
     fprintf(stderr, "Somehow, the circle could not be inserted anywhere!\n");
     abort();
   }
@@ -73,7 +102,7 @@ bool tdr::QuadTreeNode::isWithinRange(Circle& c) {
   float closestY = clamp(rcy, -halfDim, halfDim);
   float dx = rcx - closestX;
   float dy = rcy - closestY;
-  return hypot(dx, dy) < (c.r + maxRadius);
+  return hypot(dx, dy) < (c.radius + maxRadius);
 }
 
 bool tdr::QuadTreeNode::isWithinRange(Line& l) {
@@ -82,11 +111,14 @@ bool tdr::QuadTreeNode::isWithinRange(Line& l) {
   return doLinesIntersect(l1, l2);
 }
 
-bool tdr::QuadTreeNode::query(Circle& c) {
+bool tdr::QuadTreeNode::query(Circle& c, QuadTreeLeaf* st) {
   if (!isWithinRange(c)) return false;
   if (nw != nullptr) {
     for (int i = 0; i < numPoints; ++i) {
-      if (doCirclesIntersect(c, points[i])) return true;
+      if (doCirclesIntersect(c, points[i].c)) {
+        if (st != nullptr) *st = points[i];
+        return true;
+      }
     }
     return false;
   } else {
@@ -94,11 +126,14 @@ bool tdr::QuadTreeNode::query(Circle& c) {
   }
 }
 
-bool tdr::QuadTreeNode::query(Line& l) {
+bool tdr::QuadTreeNode::query(Line& l, QuadTreeLeaf* st) {
   if (!isWithinRange(l)) return false;
   if (nw != nullptr) {
     for (int i = 0; i < numPoints; ++i) {
-      if (doCircleAndLineIntersect(points[i], l)) return true;
+      if (doCircleAndLineIntersect(points[i].c, l)) {
+        if (st != nullptr) *st = points[i];
+        return true;
+      }
     }
     return false;
   } else {
