febcbed99892721061d87708d5c19ee70d04a543
bluebear94 <tkook11@gmail.com>
2016-09-22 21:53:00 -0400

    Work on CollisionRegistrar.
    This requires abandoning collision class.

--- TDR/CollisionRegistrar.h
+++ TDR/CollisionRegistrar.h
@@ -1,15 +1,32 @@
 #pragma once
 
-#include <unordered_map>
-#include <unordered_set>
+#include <functional>
+#include <vector>
 
 #include <Collidable.h>
 
 namespace tdr {
+  typedef std::function<void(Collidable*, Collidable*)> CollisionCallback;
+  struct CollisionPair {
+    Collidable* c1;
+    Collidable* c2;
+    CollisionCallback cback;
+    void perform() {
+      cback(c1, c2);
+    }
+  };
   class CollisionRegistrar {
   public:
+    template<typename C1, typename C2>
+    void registerCallback(C1* c1, C2* c2, std::function<void(C1, C2)> cback) {
+      CollisionPair p;
+      p.c1 = (Collidable) c1;
+      p.c2 = (Collidable) c2;
+      p.cback = cback;
+      callbacks.push_back(p);
+    }
   private:
-    std::unordered_multimap<int, Collidable*> collidables;
-    // std::unordered_set<
+    //std::unordered_multimap<int, Collidable*> collidables;
+    std::vector<CollisionPair> callbacks;
   };
 }
