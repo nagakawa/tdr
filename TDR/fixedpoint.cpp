054f361b51fdefbe526939c3cf83d2893447697f
bluebear94 <tkook11@gmail.com>
2016-10-13 21:04:44 -0400

    Fix warning (it was supposed to be 32-bit, not 64)

--- TDR/fixedpoint.cpp
+++ TDR/fixedpoint.cpp
@@ -23,10 +23,10 @@ fix1616 tdr::fix1616::operator-(fix1616 b) const {
 	return { u - b.u };
 }
 fix1616 tdr::fix1616::addWrap(fix1616 b) const {
-  return { (int64_t) ((uint64_t) u + (uint64_t) b.u) };
+  return { (int32_t) ((uint32_t) u + (uint32_t) b.u) };
 }
 fix1616 tdr::fix1616::subWrap(fix1616 b) const {
-  return { (int64_t) ((uint64_t) u - (uint64_t) b.u) };
+  return { (int32_t) ((uint32_t) u - (uint32_t) b.u) };
 }
 fix1616 tdr::fix1616::operator-() const {
 	if (u == ABYSS) return { CELESTIAL };
