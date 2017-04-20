22e9bd14538ef986cb68b471fbc53e53f30cf9aa
bluebear94 <tkook11@gmail.com>
2017-04-20 19:31:11 -0400

    Add Texture3

--- AGL/Texture.cpp
+++ AGL/Texture.cpp
@@ -95,3 +95,88 @@ void Texture::bindTo(GLint slot) {
 	glActiveTexture(GL_TEXTURE0 + slot);
 	bind();
 }
+
+agl::Texture3::Texture3(const char* fname, int divide, const TexInitInfo& info) {
+	int w, h;
+	if (fname == nullptr)
+		throw "File name is null!";
+	unsigned char* image = SOIL_load_image(fname, &w, &h, nullptr, SOIL_LOAD_RGBA);
+	setTexture(w, divide, h / divide, image, info);
+	SOIL_free_image_data(image);
+	//std::cerr << fname << " ~> " << id << '\n';
+}
+
+agl::Texture3::Texture3(const uint8_t* buffer, int divide, int bufferLength, const TexInitInfo& info) {
+	int w, h;
+	unsigned char* image = SOIL_load_image_from_memory(buffer, bufferLength, &w, &h, nullptr, SOIL_LOAD_RGBA);
+	setTexture(w, divide, h / divide, image, info);
+	SOIL_free_image_data(image);
+	//std::cerr << fname << " ~> " << id << '\n';
+}
+
+agl::Texture3::Texture3(int w, int h, int d, unsigned char* data, const TexInitInfo& info) {
+	setTexture(w, h, d, data, info);
+}
+
+void agl::Texture3::setTexture(int w, int h, int d, unsigned char* data, const TexInitInfo& info) {
+	changeTexture(w, h, d, data, info, true);
+}
+
+void agl::Texture3::changeTexture(int w, int h, int d, unsigned char* data, const TexInitInfo& info, bool genNew) {
+	width = w;
+	height = h;
+	depth = d;
+	GLenum mode = GL_TEXTURE_3D;
+	if (info.checkForNullData && data == nullptr)
+		throw "Image could not be read!";
+	if (genNew) glGenTextures(1, &id);
+	//std::cerr << id << " " << ms << "\n";
+	glBindTexture(mode, id);
+	//std::cerr << "done.\n";
+	if (!info.genMipMap) {
+		glTexParameteri(mode, GL_TEXTURE_BASE_LEVEL, 0);
+		glTexParameteri(mode, GL_TEXTURE_MAX_LEVEL, 0);
+	}
+	else {
+		glTexImage3D(mode, 0, info.internalFormat, w, h, d, 0, info.texFormat, info.pixelType, data);
+		glTexParameteri(mode, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(mode, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(mode, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(mode, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	}
+	if (info.genMipMap) glGenerateMipmap(mode);
+	glBindTexture(mode, 0);
+}
+
+
+/*agl::Texture::Texture(const Texture& t) {
+	id = t.id;
+	width = t.width;
+	height = t.height;
+}*/
+
+agl::Texture3::Texture3(Texture3&& t) {
+	id = t.id;
+	width = t.width;
+	height = t.height;
+	depth = t.depth;
+	t.id = 0;
+}
+
+agl::Texture3::Texture3() {
+	glGenTextures(1, &id);
+}
+
+Texture3::~Texture3() {
+	glDeleteTextures(1, &id);
+	//printf(u8"テクスチャは削除された。(%d)\n", id);
+}
+
+void Texture3::bind() {
+	glBindTexture(GL_TEXTURE_3D, id);
+}
+
+void Texture3::bindTo(GLint slot) {
+	glActiveTexture(GL_TEXTURE0 + slot);
+	bind();
+}
--- AGL/Texture.h
+++ AGL/Texture.h
@@ -39,4 +39,29 @@ namespace agl {
 		bool ms = false;
 		void setTexture(int w, int h, unsigned char* data, const TexInitInfo& info = DEFAULT_TEX_INIT);
 	};
+	class Texture3 {
+	public:
+		Texture3(const char* fname, int divide, const TexInitInfo& info = DEFAULT_TEX_INIT);
+		// Using int instead of size_t for the buffer size here, because SOIL uses
+		// that type in SOIL_load_image_from_memory. Uugghh.
+		Texture3(const uint8_t* buffer, int divide, int bufferLength, const TexInitInfo& info = DEFAULT_TEX_INIT);
+		Texture3(int w, int h, int d, unsigned char* data, const TexInitInfo& info = DEFAULT_TEX_INIT);
+		// Texture(const Texture& t);
+		Texture3(Texture3&& t);
+		Texture3();
+		~Texture3();
+		void bind();
+		void bindTo(GLint slot);
+		GLint getWidth() const { return width; }
+		GLint getHeight() const { return height; }
+		GLint getDepth() const { return depth; }
+		void changeTexture(int w, int h, int d, unsigned char* data, const TexInitInfo& info = DEFAULT_TEX_INIT, bool genNew = false);
+		GLuint id;
+	private:
+		GLint width;
+		GLint height;
+		GLint depth;
+		bool ms = false;
+		void setTexture(int w, int h, int d, unsigned char* data, const TexInitInfo& info = DEFAULT_TEX_INIT);
+	};
 }
